8f715faa6f3d2ddad108b99d2869b7b825b64c93
Pulkit Mittal <pulkitmittal@Pulkits-MacBook-Air.local>
2020-07-12 23:24:47 +0530

    first commit

---
+++ src/homepage.compnents.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './homapage.styles.scss';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>WATCHES</h1>
+                    <p>SHOP NOW</p>
+                </div>
+            </div>
+
+    
+            
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>KIDS</h1>
+                    <p>SHOP NOW</p>
+                </div>
+            </div>
+
+        
+       
+        <div className='menu-item'>
+            <div className='content'>
+                <h1>JACKETS</h1>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+
+        
+        
+        <div className='menu-item'>
+            <div className='content'>
+                <h1>WOMEN</h1>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+
+         
+        
+        <div className='menu-item'>
+            <div className='content'>
+                <h1>MEN</h1>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+
+    </div>
+    </div>
+);
+
+
+export default Homepage;
